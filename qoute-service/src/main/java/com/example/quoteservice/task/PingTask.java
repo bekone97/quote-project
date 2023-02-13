2bb93ad122ac6f846d1cd36ebc40254d851f5d53
bekone97 <myachinenergo@mail.ru>
2023-02-13 22:10:40 +0300

    Removed procfile and system.properties file

--- qoute-service/src/main/java/com/example/quoteservice/task/PingTask.java
+++ qoute-service/src/main/java/com/example/quoteservice/task/PingTask.java
@@ -2,10 +2,7 @@ package com.example.quoteservice.task;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.net.HttpURLConnection;
