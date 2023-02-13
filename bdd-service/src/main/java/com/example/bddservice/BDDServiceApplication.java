b50f0f44c6e7a78fad882831f8e2b213b61ce25a
bekone97 <myachinenergo@mail.ru>
2023-02-13 20:58:57 +0300

    changed procfile

---
+++ bdd-service/src/main/java/com/example/bddservice/BDDServiceApplication.java
@@ -0,0 +1,11 @@
+package com.example.bddservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BDDServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BDDServiceApplication.class,args);
+    }
+}
